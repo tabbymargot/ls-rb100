03ac9dbf5d2f27ade7fa3f6c148a7354bdd91c80
Caroline Wood <ec2-user@ip-172-31-34-133.eu-north-1.compute.internal>
2023-11-10 16:18:27 +0000

    added files

---
+++ ruby_basics_exercises/hashes/div_by_two.rb
@@ -0,0 +1,13 @@
+numbers = {
+  high:   100,
+  medium: 50,
+  low:    10
+}
+
+# half_numbers = {}
+
+half_numbers = numbers.map do |key, value|
+                 value / 2
+               end
+
+p half_numbers
